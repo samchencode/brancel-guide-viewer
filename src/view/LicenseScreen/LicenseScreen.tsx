969b73ba70db777c9b2825774b1c39db9c5e252a
samchencode <samchencode@gmail.com>
2023-05-23 11:24:43 -0400

    feat: add transitive dep licenses screen

--- src/view/LicenseScreen/LicenseScreen.tsx
+++ src/view/LicenseScreen/LicenseScreen.tsx
@@ -1,13 +1,32 @@
-import { theme } from '@/theme';
 import React from 'react';
-import { StyleSheet, Text, ScrollView } from 'react-native';
+import type { ListRenderItemInfo } from 'react-native';
+import { StyleSheet, Text } from 'react-native';
+import { theme } from '@/theme';
+import { licenseText } from '@/view/LicenseScreen/licenseText';
+import { FlatList } from 'react-native-gesture-handler';
+
+type LicenseSection = {
+  key: string;
+  text: string;
+};
+
+const sections = licenseText
+  .split('-----')
+  .map<LicenseSection>((t, i) => ({ key: i.toString(), text: `${t}-----` }));
+
+function renderItem({ item }: ListRenderItemInfo<LicenseSection>) {
+  return <Text style={styles.text}>{item.text}</Text>;
+}
 
 function factory() {
   return function LicenseScreen() {
     return (
-      <ScrollView style={styles.container}>
-        <Text>Licenses go here...</Text>
-      </ScrollView>
+      <FlatList
+        data={sections}
+        renderItem={renderItem}
+        keyExtractor={(item) => item.key}
+        style={styles.container}
+      />
     );
   };
 }
@@ -16,6 +35,11 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: theme.colors.background,
+  },
+  text: {
+    color: 'black',
+    fontSize: 12,
+    marginHorizontal: theme.spaces.md,
   },
 });
 
