bf74ecbdb4b8bdb3c3913eb97df27e833a6af115
samchencode <samchencode@gmail.com>
2023-05-29 12:17:01 -0400

    feat: loading text

--- src/view/HomeScreen/ProgressIndicatorView.tsx
+++ src/view/HomeScreen/ProgressIndicatorView.tsx
@@ -1,11 +1,15 @@
 import { theme } from '@/theme';
 import React from 'react';
-import { ActivityIndicator, StyleSheet, View } from 'react-native';
+import { ActivityIndicator, StyleSheet, Text, View } from 'react-native';
 
 function ProgressIndicatorView() {
   return (
     <View style={styles.container}>
       <ActivityIndicator size="large" color={theme.colors.primary} />
+      <Text style={styles.loadingText}>
+        This may take up to a minute if there is a new update to be downloaded.
+        Please keep the screen on until loading is finished.
+      </Text>
     </View>
   );
 }
@@ -16,6 +20,9 @@ const styles = StyleSheet.create({
     display: 'flex',
     justifyContent: 'center',
     alignItems: 'center',
+  },
+  loadingText: {
+    marginTop: theme.spaces.lg,
   },
 });
 
